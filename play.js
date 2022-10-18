6d0fb76585363c70091c152089746f211c925cc1
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-10-18 15:49:38 -0600

    Input Module

--- play.js
+++ play.js
@@ -1,25 +1,6 @@
 const { connect } = require('./client');
-
-let connection;
-
-const setupInput = function (conn) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  connection = conn;
-  return stdin;
-}
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') { // When you Ctrl + C
-    console.log('Connection Terminated, user quit');
-    return process.exit();;
-  }
-
-}
-setupInput();
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
+setupInput();
